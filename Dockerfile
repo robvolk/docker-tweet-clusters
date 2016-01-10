dbf659b42f606e45c2ef9c4f64bccfe3fc1b8068
Rob Volk <rob@robvolk.com>
2016-01-09 21:35:23 -0600

    Add a Redis container

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ WORKDIR /app
 ADD Gemfile /app/Gemfile
 ADD Gemfile.lock /app/Gemfile.lock
 RUN bundle install
+
+# even though we're linking the directory as a volume in docker-compose.yml, we still need to add the files
 ADD . /app
--- app.rb
+++ app.rb
@@ -1,1 +1,12 @@
-puts "aye cap'n volk here!!!!!!!"
+require "redis-objects"
+
+puts "aye, cap'n volk here!"
+
+# Since Docker uses a random IP for the container, we need to use environment variables.
+# To see which environment variables are set, run:
+# docker-compose run app env
+Redis.current = Redis.new(:host => ENV["REDIS_PORT_6379_TCP_ADDR"], :port => ENV["REDIS_PORT_6379_TCP_PORT"])
+
+@counter = Redis::Counter.new('counter_name')
+@counter.increment
+puts "Redis counter: #{@counter.value}"
